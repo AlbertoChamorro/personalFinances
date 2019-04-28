9a7213d331c2b05c8d27388de23581a51efe009b
Alberto Chamorro <albertchc04@gmail.com>
2019-04-28 13:15:55 -0600

    Create layout player

--- src/utilities/components/player-layout.js
+++ src/utilities/components/player-layout.js
@@ -7,6 +7,11 @@ export default function Player(props){
             <View style={style.containerVideo}>
                 {props.video}
             </View>
+            <View style={style.overlay}>
+                {
+                    props.isLoading && props.loader
+                }
+            </View>
         </View>
     )
 }
@@ -14,7 +19,7 @@ export default function Player(props){
 
 const style = StyleSheet.create({
     container: {
-        paddingTop: '56.25%',
+        paddingTop: '56.25%'
     },
     containerVideo: {
         position: 'absolute',
@@ -23,5 +28,15 @@ const style = StyleSheet.create({
         top: 0,
         bottom: 0,
         backgroundColor: 'black'
+    },
+    overlay: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        // backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
--- src/utilities/containers/player.js
+++ src/utilities/containers/player.js
@@ -1,20 +1,45 @@
 import React, {Component} from 'react'
-import {StyleSheet} from 'react-native'
+import {ActivityIndicator, StyleSheet} from 'react-native'
 import Video from 'react-native-video'
 import Layout from '../components/player-layout'
+import {Colors} from '../../styles/variables'
 
 export default class Player extends Component {
 
   // http://clips.vorwaerts-gmbh.de/VfE_html5.mp4
+
+  state = {
+    isLoading: true
+  }
+
+   onBuffer = ({isBuffering}) => {
+        this.setState({
+            isLoading: isBuffering
+        })
+   }
+
+   onLoad = () => {
+        this.setState({
+            isLoading: false
+        })
+    }
+
     render() {
        return( 
             <Layout
+                isLoading={this.state.isLoading}
                 video={
                     <Video source={{ uri: 'http://cbc.co/wp-content/uploads/2016/07/cbc-toma-vida-1.mp4' }}
                         style={style.video}
+                        onBuffer={this.onBuffer}
                         resizeMode="contain"
                     />
-                }>
+                }
+                loader={
+                    <ActivityIndicator style={style.indicatorLoading} 
+                        color={Colors.primary}/>
+                }
+                >
             </Layout>
        )
     }
@@ -27,5 +52,7 @@ const style = StyleSheet.create({
         right: 0,
         top: 0,
         bottom: 0,
-    }
+    },    
+    indicatorLoading: {
+    },
 })
